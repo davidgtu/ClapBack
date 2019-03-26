34497ef7b77a1b71b216c49398c232da3fef73e0
David Tu <dtu@fuzzproductions.com>
2019-03-26 16:08:12 -0400

    style textarea

--- src/components/TextInput/textarea.jsx
+++ src/components/TextInput/textarea.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import { Output } from './output';
+import styled from 'styled-components';
 
 const format = (text) => text.replace(/ /g, '👏')
-
 
 export const Textarea = () => {
   const [text, setText] = useState('');
   const [pureText, setPureText] = useState('');
-
-
+  const setTexts = (value) => {
+    setText(format(value));
+    setPureText(value);
+  }
 
   return (
-    <>
-      <textarea value={pureText} onChange={(e) => {
-        setText(format(e.target.value));
-        setPureText(e.target.target)
-      }} />
+    <Container>
+      <TextareaInput value={pureText} onChange={(e) => setTexts(e.target.value)} />
       <Output text={text} />
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  text-align: center;
+  max-width: 420px;
+  margin: 0 auto;
+`;
+
+const TextareaInput = styled.textarea`
+
+`;
