029696591a13d90017e4a9818796e20dd603134d
David Tu <dtu@fuzzproductions.com>
2019-04-01 16:51:46 -0400

    Updates

---
+++ precache-manifest.13ec072da77fbfc8c7b7393798924292.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c63c55a3290fc42fc6e",
+    "url": "/ClapBack/static/js/main.c9a88ff0.chunk.js"
+  },
+  {
+    "revision": "c13bb214f85a15d36a49",
+    "url": "/ClapBack/static/js/runtime~main.bf27bcce.js"
+  },
+  {
+    "revision": "777466c6d6d2aa43b4e4",
+    "url": "/ClapBack/static/js/2.a33231f9.chunk.js"
+  },
+  {
+    "revision": "569479bee379a1f946122d280146b819",
+    "url": "/ClapBack/index.html"
+  }
+];
